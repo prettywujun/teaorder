fc4db8debbb0265e0f4d0efb31a96b43792531d8
prettywujun <2481329853@qq.com>
2019-10-22 15:53:16 +0800

    Initial commit

---
+++ src/main/java/com/newer/service/TeaOrderService.java
@@ -0,0 +1,26 @@
+package com.newer.service;
+
+import com.newer.domain.TeaOrder;
+import com.newer.mapper.TeaOrderMapper;
+import com.newer.util.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+
+public class TeaOrderService {
+    private SqlSession sqlSession;
+    private TeaOrderMapper teaOrderMapper;
+        public void init(){
+            sqlSession= SqlSessionUtil.getSqlSession();
+            teaOrderMapper=sqlSession.getMapper(TeaOrderMapper.class);
+        }
+
+        public int addTeaOrder(TeaOrder teaOrder){
+            init();
+            int rows=teaOrderMapper.addTeaOrder(teaOrder);
+            sqlSession.commit();
+            SqlSessionUtil.close(sqlSession);
+            return rows;
+        }
+
+
+
+}
